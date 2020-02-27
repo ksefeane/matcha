b0ae098783091490fc3ba398ad33608452e4c665
ksefeane <ksefeane@student.wethinkcode.co.za>
2020-02-27 16:05:29 +0200

    solving xml

--- docs/xml/convert.py
+++ docs/xml/convert.py
@@ -1,31 +1,27 @@
 import xml.etree.ElementTree as ET 
-import csv, lxml
-import openpyxl
-from bs4 import BeautifulSoup
 
 tree = ET.parse('country_data.xml')
 root = tree.getroot()
 doc = 'test.csv'
 
-f = open('test.csv', 'w')
-cw = csv.writer(f)
 i = tree.iter()
 head = []
+data = []
+text = []
 for x in i:
 	head.append(x.tag)
-with open(doc) as f_input:
-    soup = BeautifulSoup(f_input, 'lxml')
+	data.append(x.attrib)
+	text.append(x.text)
 
-wb = openpyxl.Workbook()
-ws = wb.active
-ws.title = "sheet1"
-ws.append(head)
-body = head
-body = body.remove('data')
-for a in soup.find_all(head[0]):
-    ws.append(body)
+#for x, y, z in zip(head, data, text):
+#    print(x, y, z)
 
-for x in head:
-    print(x)        
+for x in data:
+	for y in x:
+		print(y, x[y])
 
-wb.save(filename="test.csv")
+
+
+#for x in root:
+#	print(x.tag, x.attrib)
+
